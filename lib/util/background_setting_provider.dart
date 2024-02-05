f82b1cbef9e2645716d8fce79f4cb024d287994d
kyujenius <hkj020607@gmail.com>
2024-02-05 23:53:28 +0900

    Updated: myroom_background_setting.dart New: background_stting_provider 배경 설정하는 위젯 생성(적용 x)

--- lib/util/background_setting_provider.dart
+++ lib/util/background_setting_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
   class BackgroundSettingProvider extends ChangeNotifier {
+    //MatrerailColor에서 문제점 발생
   Color selectedColor = Colors.blue;
   bool isSimpleWindowEnabled = false;
   bool isAudioSpectrumEnabled = false;
