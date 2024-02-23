27611734b9d58700ca24d33c42a54ab41f7bda04
kyujenius <hkj020607@gmail.com>
2024-02-23 17:34:41 +0900

    New: Firebase 구글 로그인 기능 구현

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mobile/pages/register.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mobile/pages/register.dart'; // 추가
 
 void main() {
   runApp(MyApp());
@@ -24,6 +25,24 @@ class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   late String _email, _password;
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    // Once signed in, return the UserCredential
+    _showDialog('Google 로그인 성공', '구글 로그인에 성공했습니다.');
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,6 +105,12 @@ class _LoginPageState extends State<LoginPage> {
               ElevatedButton(
                 onPressed: resetPasswordDialog,
                 child: Text('비밀번호 재설정'),
+              ),
+              SizedBox(height: 10.0), // 버튼과 버튼 사이에 간격 추가
+              // Google 로그인 버튼
+              ElevatedButton(
+                onPressed: signInWithGoogle, // Google 로그인 함수 호출
+                child: Text('Google로 로그인'), // 버튼 텍스트 추가
               ),
             ],
           ),
