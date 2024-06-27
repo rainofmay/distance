bc7f66a92d434bb5db7bc5193f280db3cef1240e
kyujenius <hkj020607@gmail.com>
2024-06-27 15:04:11 +0900

    [REFACTOR]: myroom_music_screen.dart 위젯 별로 묶음

--- lib/view/myroom/music/myroom_music_screen.dart
+++ lib/view/myroom/music/myroom_music_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:mobile/common/const/colors.dart';
 import 'package:mobile/view_model/myroom/music/myroom_music_view_model.dart';
@@ -36,11 +37,12 @@ class _MusicSettingState extends State<MusicSetting> {
         opacity: 0.65,
         child: SizedBox(
           width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.height * 0.70,
+          height: MediaQuery.of(context).size.height * 0.75,
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 15),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
@@ -55,140 +57,11 @@ class _MusicSettingState extends State<MusicSetting> {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 20),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              // 양 끝에 배치
-                              children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 10.0, right: 20.0),
-                                    child: Row(
-                                      children: [
-                                        Expanded(
-                                          child: Row(
-                                            children: [
-                                              Icon(CupertinoIcons.music_note_2,
-                                                  size: 16, color: WHITE),
-                                              SizedBox(width: 8),
-                                              Text(
-                                                '음악',
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.normal,
-                                                    color: WHITE),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        Icon(Icons.arrow_forward_ios,
-                                            size: 16, color: WHITE)
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15, right: 20.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Obx(() => Row(
-                                      children: [
-                                        Checkbox(
-                                          value:
-                                              musicViewModel.isVocalMusic.value,
-                                          onChanged: (value) {
-                                            musicViewModel.toggleVocalMusic();
-                                          },
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text('보컬',
-                                            style: TextStyle(color: WHITE)),
-                                      ],
-                                    )),
-                                Text('퇴근길에 듣기 좋은 팝송',
-                                    style: TextStyle(color: WHITE)),
-                                Icon(CupertinoIcons.infinite,
-                                    color: LIGHT_WHITE, size: 16),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 15, right: 20.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Obx(() => Row(
-                                      children: [
-                                        Checkbox(
-                                          value: musicViewModel
-                                              .isInstrumentalMusic.value,
-                                          onChanged: (value) {
-                                            musicViewModel
-                                                .toggleInstrumentalMusic();
-                                          },
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text('연주',
-                                            style: TextStyle(color: WHITE)),
-                                      ],
-                                    )),
-                                Text('공부할 때 듣는 피아노',
-                                    style: TextStyle(color: WHITE)),
-                                SizedBox(width: 16),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.topLeft,
-                            margin: const EdgeInsets.only(top: 30),
-                            child: Row(
-                              children: [
-                                Icon(Icons.volume_up, size: 16, color: WHITE),
-                                SizedBox(width: 8),
-                                Text(
-                                  '주변 소리',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal,
-                                      color: WHITE),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(top: 10, right: 10),
-                            height: MediaQuery.of(context).size.height * 0.30,
-                            child: SingleChildScrollView(
-                              scrollDirection: Axis.vertical,
-                              child: Obx(() {
-                                return Column(
-                                  children: musicViewModel.DUMMY_DATA[
-                                          musicViewModel
-                                              .currentGroupIndex.value]
-                                      .map((musicInfo) {
-                                    return MusicVolume(
-                                      playerIndex: musicInfo.playerIndex,
-                                      kindOfMusic: musicInfo.kindOfMusic,
-                                      musicIcon: musicInfo.musicIcon,
-                                      musicViewModel:
-                                          musicViewModel, // ViewModel을 전달
-                                    );
-                                  }).toList(),
-                                );
-                              }),
-                            ),
-                          ),
+                          musicTopBar(),
+                          const SizedBox(height: 20),
+                          mainMusics(),
+                          musicTheme(),
+                          musics()
                         ],
                       ),
                       const SizedBox(height: 20),
@@ -209,6 +82,135 @@ class _MusicSettingState extends State<MusicSetting> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget musicTopBar() {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // 양 끝에 배치
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10.0, right: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Icon(CupertinoIcons.music_note_2,
+                            size: 16, color: WHITE),
+                        SizedBox(width: 8),
+                        Text(
+                          '음악',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight:
+                              FontWeight.normal,
+                              color: WHITE),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Icon(Icons.arrow_forward_ios,
+                      size: 16, color: WHITE)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget mainMusics() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20.0),
+      child: Obx(() => Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: musicViewModel.isInstrumentalMusic.value,
+                        onChanged: (value) {
+                          musicViewModel.toggleInstrumentalMusic();
+                        },
+                      ),
+                      const SizedBox(width: 5),
+                      Text('연주', style: TextStyle(color: WHITE)),
+                    ],
+                  ),
+                  Text('공부할 때 듣는 피아노', style: TextStyle(color: WHITE)),
+                  SizedBox(width: 16),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: musicViewModel.isVocalMusic.value,
+                        onChanged: (value) {
+                          musicViewModel.toggleVocalMusic();
+                        },
+                      ),
+                      const SizedBox(width: 5),
+                      Text('보컬', style: TextStyle(color: WHITE)),
+                    ],
+                  ),
+                  Text('퇴근길에 듣기 좋은 팝송', style: TextStyle(color: WHITE)),
+                  Icon(CupertinoIcons.infinite, color: LIGHT_WHITE, size: 16),
+                ],
+              )
+            ],
+          )),
+    );
+  }
+  Widget musicTheme() {
+    return Container(
+      alignment: Alignment.topLeft,
+      margin: const EdgeInsets.only(top: 30),
+      child: Row(
+        children: [
+          Icon(Icons.volume_up, size: 16, color: WHITE),
+          SizedBox(width: 8),
+          Text(
+            '주변 소리',
+            style: TextStyle(
+                fontSize: 15, fontWeight: FontWeight.normal, color: WHITE),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget musics() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, right: 10),
+      height: MediaQuery.of(context).size.height * 0.27,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Obx(() {
+          return Column(
+            children: musicViewModel
+                .DUMMY_DATA[musicViewModel.currentGroupIndex.value]
+                .map((musicInfo) {
+              return MusicVolume(
+                playerIndex: musicInfo.playerIndex,
+                kindOfMusic: musicInfo.kindOfMusic,
+                musicIcon: musicInfo.musicIcon,
+                musicViewModel: musicViewModel, // ViewModel을 전달
+              );
+            }).toList(),
+          );
+        }),
       ),
     );
   }
