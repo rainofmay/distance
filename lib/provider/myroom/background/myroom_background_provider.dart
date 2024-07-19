a815fdad9a78a9d0a26fab0e8794d6f61f25cc73
kyujenius <hkj020607@gmail.com>
2024-07-19 23:58:25 +0900

    [FEATURE]: background provider, repository 구현

---
+++ lib/provider/myroom/background/myroom_background_provider.dart
@@ -0,0 +1,39 @@
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class MyroomBackgroundProvider {
+  static final supabase = Supabase.instance.client;
+
+  Future<List<Map<String, dynamic>>> getThemePictures(String category) async {
+    final response;
+    try{
+       response = await supabase
+          .from('background') // 테이블 이름
+          .select("*")
+          .eq('category', category) // 카테고리 필터링
+          .eq('is_image', true); // isImage 값이 false인 것만 필터링
+      print('[BackgroundProvider]: $response');
+    }
+    catch(e){
+      throw Exception('Error fetching data: ${e}');
+    }
+
+    return response;
+  }
+
+  Future<List<Map<String, dynamic>>> getThemeVideos(String category) async {
+    final response;
+    try{
+      response = await supabase
+          .from('background') // 테이블 이름
+          .select()
+          .eq('category', category) // 카테고리 필터링
+          .eq('is_image', false); // isImage 값이 false인 것만 필터링
+    }
+    catch(e){
+      throw Exception('Error fetching data: ${e}');
+    }
+
+    return response;
+  }
+}
---
+++ lib/repository/myroom/background/myroom_background_repository.dart
@@ -0,0 +1,37 @@
+
+import 'package:mobile/model/background_model.dart';
+import 'package:mobile/provider/myroom/background/myroom_background_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class MyroomBackgroundRepository {
+  late final MyroomBackgroundProvider _backgroundProvider;
+  static final supabase = Supabase.instance.client;
+
+  MyroomBackgroundRepository({
+    required MyroomBackgroundProvider backgroundProvider,
+  }) : _backgroundProvider = backgroundProvider;
+
+  onInit() {
+
+  }
+
+  //테마 사진들 불러오기
+  Future<List<ThemePicture>> fetchThemePictures(String category) async {
+    final response = await _backgroundProvider.getThemePictures(category);
+
+    // 사용자 정보를 ThemePicture 객체로 변환
+    final themePictures = ThemePicture.fromJsonList(response);
+
+    return themePictures;
+  }
+
+  //테마 영상들 불러오기
+  Future<List<ThemeVideo>> fetchThemeVideos(String category) async {
+    final response = await _backgroundProvider.getThemeVideos(category);
+
+    // 사용자 정보를 UserModel 객체로 변환
+    final themeVideos = ThemeVideo.fromJsonList(response);
+
+    return themeVideos;
+  }
+}
